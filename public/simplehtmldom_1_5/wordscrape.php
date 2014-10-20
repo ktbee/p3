d1f1320d176eaa3510b2a5ae92eafb9be0efd97f
ktbee <kbroida@gmail.com>
2014-10-20 19:47:40 -0400

    Created array with loren ipsum

--- public/simplehtmldom_1_5/wordscrape.php
+++ public/simplehtmldom_1_5/wordscrape.php
@@ -3,27 +3,38 @@
 error_reporting(E_ALL);      
 ini_set('display_errors', 1);
 
-
 require "simple_html_dom.php";
 $lolArray = [];
 
 function remove_link($element) {
-	        if ($element->tag=='a')
-	                $element->outertext = '';
+	       if ($element->tag=='a')
+	                $element->outertext = '';        
 } // gets rid of links
 
-for ($i=1; $i <= 2; $i++) { 
-	$html = file_get_html('http://www.lolcatbible.com/index.php?title=Mark_'.$i);
-	$html->set_callback('remove_link');
-	foreach($html->find('p') as $e) 
-	    $wordsToAdd = $e->innertext;
-	    array_push($lolArray, $wordsToAdd); 
-}; // grabs the innertext of p elements and adds the to an array
+function remove_span($element) {
+	       if ($element->tag=='span')
+	                $element->outertext = '';        
+}
+
+for ($i=1; $i <= 5; $i++) {
+	$lolHTML = file_get_html('http://www.lolcatbible.com/index.php?title=Mark_'.$i); 	
+	$lolHTML->set_callback('remove_link');
+	foreach ($lolHTML->find('p') as $s) {
+		$sentence = $s->innertext;
+		array_push($lolArray, $sentence);
+	}	
+}; // grabs the innertext of <span> elements and adds the to an array
+
+
+$lorenHTML = file_get_html('http://la.wikisource.org/wiki/De_finibus_bonorum_et_malorum/Liber_Primus');
+$lorenHTML->set_callback('remove_link');	
+$lorenHTML->set_callback('remove_span');
+foreach ($lorenHTML->find('p') as $p) {
+	$paragraph = $p->innertext;
+	array_push($lolArray, $paragraph);
+}
 
 print_r($lolArray);
-
-
-
 
 	
 
